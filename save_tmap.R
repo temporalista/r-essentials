ed28f8ea29cb3d701be45a400357552c6ab70726
Daniel Orellana <danielorellanav@gmail.com>
2023-02-10 09:29:49 -0500

    Create save_tmap.R

---
+++ save_tmap.R
@@ -0,0 +1,23 @@
+mySaveTmap <- function(filename = default_name(plot),
+                       height = 12,
+                       width = 15,
+                       units = "cm",
+                       dpi = 300,
+                       ...) {
+  tmap_save(
+    tm = map1,
+    insets_tm = map2,
+    insets_vp = viewport(
+      x = 0.5,
+      y = 0.15,
+      width = 0.4,
+      height = 0.25
+    ),
+    width = width,
+    height = height,
+    units = "cm",
+    dpi = dpi,
+    filename = paste0(dir,filename)
+  )
+  
+}
